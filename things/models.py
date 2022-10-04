dbf516be42946836c31d9e8ca0dd206cba8beaae
Jiale Yang <jiale.2.yang@kcl.ac.uk>
2022-10-04 17:46:00 +0100

    make refinement on model

--- things/models.py
+++ things/models.py
@@ -8,4 +8,4 @@ class Thing(models.Model):
 
     name = models.CharField(max_length=30, unique = True)
     description = models.CharField(max_length = 120, unique = False, blank = True)
-    quantity = models.PositiveSmallIntegerField(unique = False, validators=[MaxValueValidator(100), MinValueValidator(1)])
+    quantity = models.PositiveSmallIntegerField(unique = False, validators=[MaxValueValidator(100), MinValueValidator(0)])
